ea2bf3ecee49ea9d211c742c2c538e5ccd402c66
mglatki <mglatki@gmail.com>
2021-01-30 18:16:56 +0100

    JS moved to separate file

---
+++ main.js
@@ -0,0 +1,29 @@
+    let closeMenuButton = document.querySelector(".close_menu__button");
+    let openMenuButton = document.querySelector(".open_menu__button");
+    let menuSec = document.querySelector(".menu");
+    let headerEl = document.querySelector("header");
+    let footerEl = document.querySelector("footer");
+
+    /* let myPromise = new Promise(function(myResolve, myReject) {
+      setTimeout(function(){ myResolve("Hello !!"); }, 3000);
+    });
+
+    myPromise.then(function(value) {
+      document.getElementById("demo").innerHTML = value;
+    }); */
+
+    openMenuButton.addEventListener('click', () => {
+      headerEl.classList.toggle('header--expanded');
+      menuSec.classList.toggle('menu--visible');
+      footerEl.classList.toggle('footer--invisible');
+      closeMenuButton.classList.toggle('close_menu__button--visible');
+      openMenuButton.classList.toggle('open_menu__button--invisible');
+    })
+
+    closeMenuButton.addEventListener('click', () => {
+      headerEl.classList.toggle('header--expanded');
+      menuSec.classList.toggle('menu--visible');
+      footerEl.classList.toggle('footer--invisible');
+      closeMenuButton.classList.toggle('close_menu__button--visible');
+      openMenuButton.classList.toggle('open_menu__button--invisible');
+    })
